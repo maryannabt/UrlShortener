bc00e3228ce2227c09164786f7045e6155b3f9e6
netzcat <netzzzcat@gmail.com>
2021-01-17 17:14:12 +0200

    Fixed ErrorModal

--- client/src/components/ErrorModal.js
+++ client/src/components/ErrorModal.js
@@ -1,32 +1,44 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 import M from "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 
-const ErrorModal = props => {
-  const myModal = useRef();
+const ErrorModal = (props) => {
+  const errorModal = useRef();
   const { onClear, error } = props;
 
   useEffect(() => {
     const options = {
-        inDuration: 250,
-        outDuration: 250,
-        opacity: 0.5,
-        dismissible: true,
-        startingTop: "4%",
-        endingTop: "10%"
-      };
+      onCloseEnd: () => {
+        onClear();
+      },
+      inDuration: 250,
+      outDuration: 250,
+      opacity: 0.5,
+      dismissible: true,
+      startingTop: "4%",
+      endingTop: "10%"
+    };
 
-      M.Modal.init(myModal, options);
-  }, []);
-  
+    let modalInstance = M.Modal.init(errorModal.current, options);
+    modalInstance.open();
+
+    return () => {
+      modalInstance.close();
+    };
+  }, [onClear]);
+
   return (
-    <div ref={myModal} id="modal1" className="modal">
+    <div ref={errorModal} id="modal1" className="modal">
       <div className="modal-content">
         <h4>An Error Occurred!</h4>
         <p>{error}</p>
       </div>
       <div className="modal-footer">
-        <a href="#!" className="modal-close waves-effect waves-green btn-flat" onClick={onClear}>
+        <a
+          href="#!"
+          className="modal-close waves-effect waves-green btn-flat"
+          onClick={onClear}
+        >
           OK
         </a>
       </div>

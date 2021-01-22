4de942b93414f89c9d07fbd834808b0a807c7806
netzcat <netzzzcat@gmail.com>
2021-01-22 17:51:52 +0200

    Finished links controllers functions for the link routes

--- controllers/links-controllers.js
+++ controllers/links-controllers.js
@@ -3,6 +3,7 @@ const config = require('config');
 const shortid = require('shortid');
 
 const Link = require('../models/Link');
+const User = require('../models/User');
 const HttpError = require('../models/http-error');
 
 const generateLink = async (req, res, next) => {
@@ -11,15 +12,94 @@ const generateLink = async (req, res, next) => {
     if (!errors.isEmpty()) {
         return next(new HttpError('Invalid inputs passed, please check your data.', 422));
     }
+
+    const { from } = req.body;
+
+    let existingLink;
+    try {
+        existingLink = await Link.findOne({ from });
+    } catch (err) {
+        const error = new HttpError('Generating link failed, please try again.', 500);
+        return next(error);
+    }
+
+    if (existingLink) {
+        return res.json({ link: existingLink });
+    }
+
+    const baseUrl = config.get('baseUrl');
+    const code = shortid.generate();
+    const to = baseUrl + '/t/' + code;
+
+    const createdLink = new Link({
+        from, to, code, creator: req.userData.userId
+    });
+
+    let user;
+    try {
+        user = await User.findById(req.userData.userId);
+    } catch (err) {
+        const error = new HttpError('Generating link failed, please try again.', 500);
+        return next(error);
+    }
+
+    if (!user) {
+        const error = new HttpError('Could not find user for provided id.', 404);
+        return next(error);
+    }
+
+    try {
+        const sess = await mongoose.startSession();
+        sess.startTransaction();
+        await createdLink.save({ session: sess });
+        user.links.push(createdLink);
+        await user.save({ session: sess });
+        await sess.commitTransaction();
+      } catch (err) {
+        const error = new HttpError('Generating link failed, please try again.', 500);
+        return next(error);
+      }
     
+      res.status(201).json({ link: createdLink });
 };
 
 const getLinks = async (req, res, next) => {
-    
+    let userWithLinks;
+    try {
+        userWithLinks = await User.findById(req.userData.userId).populate('links');
+    } catch (err) {
+        const error = new HttpError('Fetching links failed, please try again later.', 500);
+        return next(error);
+    }
+
+    if (!userWithLinks || userWithLinks.links.length === 0) {
+        return next(new HttpError('Could not find links for the provided user id.', 404));
+    }
+
+    res.json({
+        links: userWithLinks.links.map(link =>
+        link.toObject({ getters: true })
+        )
+    });
 };
 
 const getLinkById = async (req, res, next) => {
-    
+    const linkId = req.params.id;
+
+    let link;
+    try {
+        link = await Link.findById(linkId);
+    } catch (err) {
+        const error = new HttpError('Something went wrong, could not find a link.', 500);
+        return next(error);
+    }
+
+    if (!link) {
+        const error = new HttpError('Could not find link for the provided id.', 404);
+        return next(error);
+    }
+
+    res.json({ link: link.toObject({ getters: true }) });
 };
 
 exports.generateLink = generateLink;
--- models/User.js
+++ models/User.js
@@ -1,9 +1,12 @@
 const { Schema, model, Types } = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     links: [{ type: Types.ObjectId, ref: 'Link '}]
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = model('User', userSchema);
